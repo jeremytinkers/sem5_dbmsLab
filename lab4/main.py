1d23ec7fc3dc9092b025de9058eb450b3a1e5143
Jeremiah Thomas <jtreplies@gmail.com>
2021-11-03 23:07:37 +0400

    Updated Lab4: Working with Python and MySQL

---
+++ lab4/main.py
@@ -0,0 +1,14 @@
+
+import deleteModule
+import findModule
+import updateModule
+import insertModule
+
+
+#For updating:- 
+updateModule.updateFn()
+
+#Similary for the rest:-
+#deleteModule.deleteFn()
+#findModule.findFn()
+#insertModule.insertFn()
